a0deea06097238ebf0b2af39d54f0812a11af949
EC2 Default User <ec2-user@ip-172-31-21-23.ap-northeast-1.compute.internal>
2023-04-30 13:39:42 +0000

    [Finish]3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,7 +10,7 @@ class ListsController < ApplicationController
     else
       render :new
     end
-  end  
+  end
 
   def index
     @lists = List.all
@@ -23,18 +23,18 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     redirect_to '/lists'
-  end  
+  end
 
   private
 
